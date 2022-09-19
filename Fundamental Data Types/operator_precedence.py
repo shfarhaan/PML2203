94db5687213ee1c62c562bb9dcbb2a3bc4be49d0
shfarhaan <farhaan.hussain21@gmail.com>
2022-09-19 17:21:25 +0600

    Operator Precedence Update

--- Fundamental Data Types/operator_precedence.py
+++ Fundamental Data Types/operator_precedence.py
@@ -8,5 +8,15 @@
 
 # '()' > '**'> '* /' > '+ -'
 
-print((20 - 3) + 2 ** 3 )
+# Guess the output of each answer before you click RUN
+# Try to write down your answer before and see how you do... keep it mind I made it a little tricky for you :)
 
+print((5 + 4) * 10 / 2)
+
+print(((5 + 4) * 10) / 2)
+
+print((5 + 4) * (10 / 2))
+
+print(5 + (4 * 10) / 2)
+
+print(5 + 4 * 10 // 2)
