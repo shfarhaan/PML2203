be5c559c0589f2f5ebb1d79e199639e4ed894db0
shfarhaan <farhaan.hussain21@gmail.com>
2022-09-19 17:19:42 +0600

    Operator Precedence

---
+++ Fundamental Data Types/operator_precedence.py
@@ -0,0 +1,12 @@
+# Operator Precedence Follows PEMDAS Rule
+# P = Parenthesis
+# E = Exponentiation
+# M = Multiplication
+# D = Division
+# A = Addition
+# S = Subtraction
+
+# '()' > '**'> '* /' > '+ -'
+
+print((20 - 3) + 2 ** 3 )
+
