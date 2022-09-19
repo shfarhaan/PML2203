b42dfa21640c747acfe8b41fb66f253ba204a133
shfarhaan <farhaan.hussain21@gmail.com>
2022-09-19 16:29:04 +0600

    DataTypes: Numbers

--- Fundamental Data Types/numbers.py
+++ Fundamental Data Types/numbers.py
@@ -14,9 +14,16 @@ print(type(2 - 4))
 print(type(2 * 4))
 print(type(2 / 4))   # 0.5 this is a floating point number
 
-print("\nDemonstrating the other mathematical functions...\n")
+print("\nDemonstrating the other mathematical Operations...\n")
 print(2**4)   # Exponentiation
 print(6 // 4)   # Double Divide
 print(6 % 4)   # giving the remainder
 
+print("\nDemonstrating the other mathematical functions...\n")
+print(round(3.9))    # Rounding Function
+print(abs(-20))     # Absolute Value i.e. making any negative value positive.
 
+
+# To know more about Math Functions we can
+# check Python Docs and search for
+# Python Math Functions
