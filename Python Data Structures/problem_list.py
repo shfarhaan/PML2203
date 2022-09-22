6695c2e24e9734eb18c3cc2bd357733d3386980c
shfarhaan <farhaan.hussain21@gmail.com>
2022-09-22 14:50:47 +0600

    problem list v1

---
+++ Python Data Structures/problem_list.py
@@ -0,0 +1,18 @@
+import random
+
+practice = range(1, 282)
+
+practice_list = random.sample(practice, k = 50)
+so_li = sorted(practice_list)
+print(f"Sorted practice list: {so_li}")
+
+print(f"Length of practice list: {len(practice_list)}")
+print(f"Length of sorted practice list: {len(so_li)}")
+
+
+# n = 50
+# for each_person in range(n):
+#     Azwad.append(random.sample)
+    # print(f"Azwad will solve problems and these are {Azwad.append(each_person)}")
+#
+# print(Azwad)
