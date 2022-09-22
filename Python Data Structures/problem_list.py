96a2b91a60abc3fcc77fb7a724db4a440bd4c273
shfarhaan <farhaan.hussain21@gmail.com>
2022-09-22 17:14:42 +0600

    problem list v2 unsolved

--- Python Data Structures/problem_list.py
+++ Python Data Structures/problem_list.py
@@ -1,18 +1,35 @@
 import random
 
-practice = range(1, 282)
+problem_range = range(1, 282)
+Students = ["A", "B", "C", "D", "E"]
+# TO_BE_SOLVED = []
 
-practice_list = random.sample(practice, k = 50)
-so_li = sorted(practice_list)
-print(f"Sorted practice list: {so_li}")
+for each_person in Students:
+    TO_BE_SOLVED = []
+    for problems_to_solved in TO_BE_SOLVED:
+        TO_BE_SOLVED = random.sample(problem_range, k=50)
 
-print(f"Length of practice list: {len(practice_list)}")
-print(f"Length of sorted practice list: {len(so_li)}")
 
+
+
+
+
+
+
+
+
+
+
+
+# so_li = sorted(practice_list)
+# print(f"Sorted practice list: {so_li}")
+#
+# print(f"Length of practice list: {len(practice_list)}")
+# print(f"Length of sorted practice list: {len(so_li)}")
 
 # n = 50
 # for each_person in range(n):
 #     Azwad.append(random.sample)
-    # print(f"Azwad will solve problems and these are {Azwad.append(each_person)}")
+# print(f"Azwad will solve problems and these are {Azwad.append(each_person)}")
 #
 # print(Azwad)
