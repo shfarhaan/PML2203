8048e1a429979add668f9e2c2ee54464bdd79f57
shfarhaan <farhaan.hussain21@gmail.com>
2022-09-23 23:47:33 +0600

    problem25 from the problem set of lists in w3_Resources

--- Random Programs/problem_distributor_function.py
+++ Random Programs/problem_distributor_function.py
@@ -1,11 +1,11 @@
 import random
 
 
-def problems_to_be_solved(seed_value, name):
+def problems_to_be_solved(seed_value, name, no_of_problems, sample):
     random.seed(seed_value)
-    practice = range(1, 282)
+    practice = range(1, no_of_problems + 1)
 
-    problem_list = random.sample(practice, k=50)
+    problem_list = random.sample(practice, k=sample)
     to_be_solved_list = sorted(problem_list)
     print(f"{name} is going to solve this list: {to_be_solved_list}")
 
@@ -13,10 +13,10 @@ def problems_to_be_solved(seed_value, name):
     print(f"Length of sorted practice list: {len(to_be_solved_list)}")
 
 
-problems_to_be_solved(100, "Farhaan")
-problems_to_be_solved(29, "Azwad")
-problems_to_be_solved(57, "Shuvom")
-problems_to_be_solved(70, "Samuel")
-problems_to_be_solved(13, "Showrin")
-
-
+#
+# problems_to_be_solved(100, "Farhaan")
+# problems_to_be_solved(29, "Azwad")
+# problems_to_be_solved(57, "Shuvom")
+# problems_to_be_solved(70, "Samuel")
+problems_to_be_solved(20, "Showrin", 281, 50)
+problems_to_be_solved(20, "Showrin", 80, 40)
---
+++ w3resources problems/list problems/problem25.py
@@ -0,0 +1,6 @@
+# 25. Write a Python program to select
+# an item randomly from a list.
+
+import random
+color_list = ['Pikachu', 'Boulbasaur', 'Charmander', 'Squirtle', 'Magicarp']
+print(random.choice(color_list))
