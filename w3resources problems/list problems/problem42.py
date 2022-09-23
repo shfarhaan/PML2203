04f263e499e2234f0823d7a884e31b09b13bf6be
shfarhaan <farhaan.hussain21@gmail.com>
2022-09-23 23:54:36 +0600

    problem42

---
+++ w3resources problems/list problems/problem42.py
@@ -0,0 +1,6 @@
+# 42. Write a Python program to find
+# missing and additional values in two lists.
+#
+# Sample data : Missing values in second list: b,a,c
+# Additional values in second list: g,h
+
