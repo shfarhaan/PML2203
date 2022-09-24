b278599a0fc88a7b5d3b93b1740d5c18bf30e3c8
shfarhaan <farhaan.hussain21@gmail.com>
2022-09-24 10:18:47 +0600

    problem27

--- w3resources problems/list problems/problem27.py
+++ w3resources problems/list problems/problem27.py
@@ -1,3 +1,9 @@
 # 27. Write a Python program to find
-# the second smallest number in a list.
+# the second-smallest number in a list.
 
+# list_of_fruits = ["Water Melon", "Pears", "Avocado", "Jackfruit", "Pomegranate"]
+
+list_of_numbers = [100, 34, 399, 10283, 3, 4, 10, -204, 10]
+sorted_list = sorted(list_of_numbers)
+
+print((sorted_list[1]))
