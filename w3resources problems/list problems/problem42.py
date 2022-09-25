91125c162ab851df9dcbea5df826d47e0e6c8380
shfarhaan <farhaan.hussain21@gmail.com>
2022-09-25 15:17:14 +0600

    problem42

--- w3resources problems/list problems/problem42.py
+++ w3resources problems/list problems/problem42.py
@@ -8,5 +8,9 @@ no_of_list = int(input("How many list do you want to check? "))
 
 for i in range(1, no_of_list+1):
     elements_in_list = int(input("Enter number of Elements: "))
-    list_of_elements = list(map(int, input("\nEnter the numbers : ").strip().split()))[:no_of_list]
-    print("\n List of Items in the list")
+    list_of_elements = list(map(int, input("\nEnter the numbers : ")
+                                .strip().split()))[:elements_in_list]
+    print(f"\nList of Items in the list - {list_of_elements}")
+
+
+
