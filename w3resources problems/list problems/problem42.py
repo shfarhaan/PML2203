b1e7e5d90aa755582e6d60e42d989362a9ee62b9
shfarhaan <farhaan.hussain21@gmail.com>
2022-09-25 13:03:53 +0600

    problem42

--- w3resources problems/list problems/problem42.py
+++ w3resources problems/list problems/problem42.py
@@ -4,3 +4,9 @@
 # Sample data : Missing values in second list: b,a,c
 # Additional values in second list: g,h
 
+no_of_list = int(input("How many list do you want to check? "))
+
+for i in range(1, no_of_list+1):
+    elements_in_list = int(input("Enter number of Elements: "))
+    list_of_elements = list(map(int, input("\nEnter the numbers : ").strip().split()))[:no_of_list]
+    print("\n List of Items in the list")
