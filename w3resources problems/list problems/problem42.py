a5c8c530fd580523f72d4ff970599c0d5e2093f7
shfarhaan <farhaan.hussain21@gmail.com>
2022-09-25 19:19:42 +0600

    problem42

--- w3resources problems/list problems/problem42.py
+++ w3resources problems/list problems/problem42.py
@@ -1,16 +1,23 @@
 # 42. Write a Python program to find
 # missing and additional values in two lists.
-#
+
 # Sample data : Missing values in second list: b,a,c
 # Additional values in second list: g,h
 
-no_of_list = int(input("How many list do you want to check? "))
+elements_in_list1 = int(input("Enter number of Elements in List 1: "))
+lis1 = list(map(str, input("\nEnter the numbers : ")
+                .strip().split()))[:elements_in_list1]
 
-for i in range(1, no_of_list+1):
-    elements_in_list = int(input("Enter number of Elements: "))
-    list_of_elements = list(map(int, input("\nEnter the numbers : ")
-                                .strip().split()))[:elements_in_list]
-    print(f"\nList of Items in the list - {list_of_elements}")
+elements_in_list2 = int(input("Enter number of Elements in List2: "))
+lis2 = list(map(str, input("\nEnter the numbers : ")
+                .strip().split()))[:elements_in_list2]
 
+print(f"\nList of Items in the list 1 - {lis1}\n"
+      f"and List of Items in the list 2 - {lis2}\n")
 
+uniq_lis1 = set(lis1)
+uniq_lis2 = set(lis2)
+
+print(f"Missing values in second list: {uniq_lis1 - uniq_lis2}")
+print(f"Additional values in second list: {uniq_lis2 - uniq_lis1}")
 
