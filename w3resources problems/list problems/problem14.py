2d4669516e44c7484b8c7f2c9cc01daf2dd57e70
shfarhaan <farhaan.hussain21@gmail.com>
2022-09-25 14:58:50 +0600

    problem14

--- w3resources problems/list problems/problem14.py
+++ w3resources problems/list problems/problem14.py
@@ -3,14 +3,13 @@
 # removing even numbers from it.
 
 inputs = int(input("Please enter how many elements "
-               "you want to insert in the list: \n\t\t"))
+                   "you want to insert in the list: \n\t\t"))
 
 specified_list = []
-for each_input in range(1, inputs+1):
+for each_input in range(1, inputs + 1):
     num = int(input(f"The no.{each_input} in your specified list is: "))
     specified_list.append(num)
     if num % 2 == 0:
         specified_list.remove(num)
-
 
 print(f"The specified list without any even numbers is: {specified_list}")
