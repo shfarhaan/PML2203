3920879ccf976eeb30eae043f9a8dd6b6896fec0
shfarhaan <farhaan.hussain21@gmail.com>
2022-09-25 14:57:32 +0600

    yet not solved

--- Random Programs/awesome_number.py
+++ Random Programs/awesome_number.py
@@ -6,19 +6,26 @@
 # its immediate left digit is called an Awesome number. A single-digit number
 # cannot be an awesome number (e.g. 5421 is an Awesome number.)
 
-
-# def awesome_number(number):
 input_number = input("Input a number of your choice to \n"
                      "check if the number is awesome or not... \n\t\t")
 
-number_in_a_list = [int(index) for index in str(input_number)]
+num_list = [int(digit) for digit in input_number]
 
-print(number_in_a_list)
-print(number_in_a_list[-1])
-print(number_in_a_list[0:-1])
-# for i in number_in_a_list:
+for i in num_list:
+    if num_list[:-1] < num_list[0:]:
+        i += 1
+        print(i)
 
-if list(number_in_a_list[:-1]) < number_in_a_list:
-    print("Awesome Number")
-else:
-    print("Not an Awesome Number!")
+
+#
+# print(type(num_list))
+
+
+# number_in_a_list = [int(index) for index in str(input_number)]
+# num_to_string =
+#
+#
+# if check(number_in_a_list, ):
+#     print("Awesome Number")
+# else:
+#     print("Not an Awesome Number")
