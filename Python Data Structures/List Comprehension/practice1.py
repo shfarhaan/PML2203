7dd27c9fbfbefc9e5432ec11d8d7e3bda3fc2e4d
shfarhaan <farhaan.hussain21@gmail.com>
2022-09-26 12:56:01 +0600

    practice1 ex2

--- Python Data Structures/List Comprehension/practice1.py
+++ Python Data Structures/List Comprehension/practice1.py
@@ -1,18 +1,31 @@
-a = [1, 3, 5, 7, 9, 11]
-
-# We want a list [2, 6, 10, 14, 18, 22]
-# that has the doubled value of elements in list a.
+# a = [1, 3, 5, 7, 9, 11]
+#
+# # We want a list [2, 6, 10, 14, 18, 22]
+# # that has the doubled value of elements in list a.
+#
+# # USING REGULAR FOR LOOP
+# b = []
+#
+# for each_element in a:
+#     b.append(each_element*2)
+#
+# print(b)
+#
+# # USING LIST COMPREHENSION
+# c = [each_element * 2 for each_element in a]
+#
+# print(c)
 
 # USING REGULAR FOR LOOP
-b = []
+ele = []
 
-for each_element in a:
-    b.append(each_element*2)
+for each_element in range(1,7):
+    ele.append(each_element ** 2)
 
-print(b)
+print(ele)
 
 # USING LIST COMPREHENSION
-c = [each_element * 2 for each_element in a]
+ele2 = [each_elem ** 2 for each_elem in range(1,7)]
 
-print(c)
-#
+print(ele2)
+
