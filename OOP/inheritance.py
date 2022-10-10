ea8a679a0067114481f1f0d8112cb39f6d1fc908
shfarhaan <farhaan.hussain21@gmail.com>
2022-10-10 19:31:36 +0600

    inheritance s

--- OOP/inheritance.py
+++ OOP/inheritance.py
@@ -1,27 +1,28 @@
 class A:
-    def feature1(self):
+    def __init__(self, feature1):
         print("This is feature1")
 
-    def feature2(self):
+    def feature2(self, feature2):
         print("This is feature2")
 
+
 class B:
-    def feature3(self):
+    def __init__(self, feature3):
         print("This is feature3")
 
-    def feature4(self):
+    def feature4(self, feature4):
         print("This is feature4")
 
 
 class C(A, B):
-    def feature5(self):
+    def feature5(self, feature5):
         print("This is feature5")
 
-    def feature6(self):
+    def feature6(self, feature6):
         print("This is feature6")
 
-a1 = A()
-b1 = B()
-c1 = C()
-
-
+#
+# a1 = A()
+# b1 = B()
+# c1 = C()
+#
